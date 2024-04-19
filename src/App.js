6964cf9514bc20bd6b8d0986d90efe277b8f6096
seali <sealimosavi.am@gmail.com>
2024-04-19 10:54:29 +0330

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <div className="content">
+        <Home />
       </div>
     </div>
   );
