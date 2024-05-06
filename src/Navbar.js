0376a252e51c9dc92081b37a0790042a1f8bfa57
seali <sealimosavi.am@gmail.com>
2024-05-06 15:24:06 +0330

    added

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
             <h1>The seali Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" >New Blog</a>
             </div>
         </nav>
       );
