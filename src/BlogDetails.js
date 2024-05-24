48a46699ec696bae1e2c87fc12c7ac1c9d7df18e
seali <sealimosavi.am@gmail.com>
2024-05-24 18:54:31 +0330

    blog details edited

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,31 +1,33 @@
 import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from './useFetch';
+
 const BlogDetails = () => {
     const { id } = useParams();
-const { data: blog , error, isPending} = useFetch('http://localhost:8000/blogs/'+ id)
-const history = useHistory();
+    const { data: blog, error, isPending } = useFetch(`/api/blogs/${id}`);
+    const history = useHistory();
 
-const handleClick = () => {
-    fetch('http://localhost:8000/blogs/' + blog.id, {
-        method: 'DELETE'
-    }).then(() => {
-        history.push('/');
-    })
-}
-    return ( 
+    const handleClick = () => {
+        fetch(`/api/blogs/${blog.id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        });
+    }
+
+    return (
         <div className="blog-details">
-            { isPending && <div>Loading...</div>}
-            { error && <div>{ error }</div> }
-            { blog && (
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
                 <article>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                    <div>{ blog.body }</div>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
                     <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
-     );
+    );
 }
- 
+
 export default BlogDetails;
